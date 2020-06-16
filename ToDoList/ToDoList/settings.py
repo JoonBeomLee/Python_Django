f9a93087616f6912ca9f102c6a149e7856061d6e
JoonBeomLee <jbl9595@naver.com>
2020-06-17 03:49:07 +0900

    static css 적용

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -140,6 +139,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    # add
-    os.path.join(BASE_DIR, STATIC_URL),
+    # add static file 적용
+    os.path.join(BASE_DIR, 'ToDolist', 'static'),
 )
