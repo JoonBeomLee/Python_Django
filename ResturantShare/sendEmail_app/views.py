7e55fcb8b2d12b79395f86dcd964e69ad777c9a4
JoonBeomLee <jbl9595@naver.com>
2020-06-18 03:12:47 +0900

    shareRes Project

--- ResturantShare/sendEmail_app/views.py
+++ ResturantShare/sendEmail_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from shareRes_app.models import *
 
 # SMTP Module
+from sendEmail_app.hidden import SMTP_INFO
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -11,14 +12,14 @@ from email.mime.multipart import MIMEMultipart
 # Create your views here.
 def sendEmail(request):
     checked_rsts = request.POST.getlist('checks')
-    receiver = request.POST['inputReceiver'].split(",") 
+    receiver = request.POST['inputReceiver'] 
     title = request.POST['inputTitle']
     content = request.POST['inputContent']
 
     mail_html = f"<html><body>"\
                 f"<h1>맛집 추천</h1>"\
                 f"<p> {content} <br />"\
-                f"본 사이트에서 추천한 맛집은 다음과 같습니다.</p>"
+                f"본 사이트에서 추천한 맛집은 다음과 같습니다.</p><br /><br />"
 
     for rst in checked_rsts:
         rst_obj = restaurant.objects.get(id=rst)
@@ -28,9 +29,22 @@ def sendEmail(request):
                      f"<h3>* 관련 키워드 <p>{rst_obj.rst_keyword}</p></h3><br/><br/>"
 
     mail_html += "</body></html>"
-    
-    # SMTP
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login()
 
-    return HttpResponse("sendEmail")
+    # SMTP
+    try:
+        SMTP_ID, SMTP_PWD = SMTP_INFO()
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(SMTP_ID, SMTP_PWD)
+    except:
+        print("SMTP ERROR")
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = title
+    msg['From'] = SMTP_ID
+    msg['To'] = receiver
+    mail_html = MIMEText(mail_html, 'html')
+    msg.attach(mail_html)
+    server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
+    server.quit()
+    
+    return HttpResponseRedirect(reverse('index'))
