3fd5a04401ec1268a8aafce76717984cf853332d
JoonBeomLee <jbl9595@naver.com>
2020-06-18 15:55:00 +0900

    WebExcel Project init

---
+++ WebExcel/excelCalc_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExcelcalcAppConfig(AppConfig):
+    name = 'excelCalc_app'
---
+++ WebExcel/sendEmail_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SendemailAppConfig(AppConfig):
+    name = 'sendEmail_app'
